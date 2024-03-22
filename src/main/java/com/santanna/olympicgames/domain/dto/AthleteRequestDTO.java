301e8fa5d75605adc96cb250656eaf46e5e9dd9b
LSantAnnaB <l.sant.anna.b@gmail.com>
2024-03-22 16:48:29 -0300

    dto

--- src/main/java/com/santanna/olympicgames/domain/dto/AthleteRequestDTO.java
+++ src/main/java/com/santanna/olympicgames/domain/dto/AthleteRequestDTO.java
@@ -1,4 +1,12 @@
 package com.santanna.olympicgames.domain.dto;
 
-public record AthleteResponseDTO() {
+import com.santanna.olympicgames.domain.enums.Continent;
+import com.santanna.olympicgames.domain.enums.Country;
+import com.santanna.olympicgames.domain.enums.Gender;
+import com.santanna.olympicgames.domain.enums.Sport;
+
+public record AthleteRequestDTO(
+        String name, int weight, double height, int age, Continent continent, Country country, Gender gender,
+        Sport sport
+) {
 }
