b8f506df21ac15b86c96a74f11844f248b1ef0bf
LSantAnnaB <l.sant.anna.b@gmail.com>
2024-03-22 15:19:04 -0300

    Start project

---
+++ src/main/java/com/santanna/olympicgames/OlympicGamesApplication.java
@@ -0,0 +1,13 @@
+package com.santanna.olympicgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlympicGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlympicGamesApplication.class, args);
+	}
+
+}
