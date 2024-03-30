d87e9714df143d635b8ecff1c95abb30a5bed176
LSantAnnaB <l.sant.anna.b@gmail.com>
2024-03-30 08:52:24 -0300

    refactor AthleteResponseDTO

--- src/main/java/com/santanna/olympicgames/domain/dto/GenderCountDTO.java
+++
@@ -1,8 +0,0 @@
-package com.santanna.olympicgames.domain.dto;
-
-import com.santanna.olympicgames.domain.enums.Country;
-import com.santanna.olympicgames.domain.enums.Sport;
-
-public record GenderCountDTO(Country country, Sport sport, long maleCount, long femaleCount) {
-
-}
